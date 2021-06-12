42fa1939928f844d8f67447cada69f9aab39eaa6
Study Profile <dee.alves2@gmail.com>
2021-06-12 12:31:01 -0300

    Criando modal dinamico

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,25 +1,41 @@
-import { Injectable, TemplateRef } from '@angular/core';
+import { BodyInjectorService } from './../../../services/body-injector.service';
+import { ModalComponent } from './../modal.component';
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
+import { ModalConfig } from '../interfaces/modal-config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+  private componentFactory: ComponentFactory<ModalComponent>;
 
-  constructor() { }
+  constructor(
+    componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private bodyInjector: BodyInjectorService
+  ) {
+    this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
 
   public open(config: ModalConfig): ModalRef {
+    const componentRef = this.createComponenteRef();
+    componentRef.instance.config = config;
+    console.log(componentRef.instance);
     console.log('open called');
-    return new ModalRef();
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef);
+  }
+
+  private createComponenteRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
   }
 }
 
-export interface ModalConfig {
-  templateRef: TemplateRef<any>;
-  title: string;
-}
-
 export class ModalRef {
+  constructor(private componentRef: ComponentRef<ModalComponent>) { }
+
   public close(): void {
     console.log('close called');
+    this.componentRef.destroy();
   }
 }
