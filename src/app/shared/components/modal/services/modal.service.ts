601562caa238cb89fdc00b620cbd9d9fddcb3b21
Study Profile <dee.alves2@gmail.com>
2021-06-12 00:01:56 -0300

    Criação do projeto

---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, TemplateRef } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  constructor() { }
+
+  public open(config: ModalConfig) {
+    console.log('open called');
+    return new ModalRef();
+  }
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef {
+  public close(): void {
+    console.log('close called');
+  }
+}
